a18b06c885560ce60cf2ba6bbb77c1502ded4f9c
dereekb <dereekb@gmail.com>
2015-07-27 16:06:32 -0500

    Added ClearLinksTask

--- gae/src/main/java/com/dereekb/gae/utilities/collections/IteratorUtility.java
+++ gae/src/main/java/com/dereekb/gae/utilities/collections/IteratorUtility.java
@@ -1,10 +1,17 @@
 package com.dereekb.gae.utilities.collections;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
-
+/**
+ * Utility for {@link Iterable} values.
+ *
+ * @author dereekb
+ *
+ */
 public class IteratorUtility {
 
 	public static <T> List<T> iterableToList(Iterable<T> iterable) {
@@ -20,6 +27,21 @@ public class IteratorUtility {
 		}
 
 		return list;
+	}
+
+	public static <T> Set<T> iterableToSet(Iterable<T> iterable) {
+		Set<T> set = new HashSet<T>();
+
+		if (iterable != null) {
+			Iterator<T> iterator = iterable.iterator();
+
+			while (iterator.hasNext()) {
+				T element = iterator.next();
+				set.add(element);
+			}
+		}
+
+		return set;
 	}
 
 }
