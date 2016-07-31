a1b8f2758860663fd08874b012ddda610cd5dc91
Derek Burgman <dereekb@gmail.com>
2016-07-31 02:22:01 -0500

    Updated Dependency Version
    - Updated dependency version of Hibernate
    - Updated StringUtility

--- gae/src/main/java/com/dereekb/gae/utilities/data/StringUtility.java
+++ gae/src/main/java/com/dereekb/gae/utilities/data/StringUtility.java
@@ -13,4 +13,32 @@ public class StringUtility {
 		return length;
 	}
 
+	public static String createStringWithLength(int length) {
+		String string = "";
+
+		int r = length % 10;
+
+		if (length > 10) {
+			int c = (length - r) / 10;
+			string = copyString("XXXXXXXXXX", c);
+		}
+
+		if (r > 0) {
+			string += copyString("X", r);
+		}
+
+		return string;
+	}
+
+	public static String copyString(String copy,
+	                                int count) {
+		StringBuilder builder = new StringBuilder();
+
+		for (int i = 0; i < count; i += 1) {
+			builder.append(copy);
+		}
+
+		return builder.toString();
+	}
+
 }
