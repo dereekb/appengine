0f92a5912735e13d5a5a76b1d20b8feb1dbba597
Derek Burgman <dereekb@gmail.com>
2017-03-25 03:22:25 -0500

    Updated ObjectifyDatabaseTest

--- gae/src/test/java/com/dereekb/gae/test/applications/api/model/tests/database/ObjectifyDatabaseTest.java
+++ gae/src/test/java/com/dereekb/gae/test/applications/api/model/tests/database/ObjectifyDatabaseTest.java
@@ -1,17 +1,17 @@
 package com.dereekb.gae.test.applications.api.model.tests.database;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 import com.dereekb.gae.server.auth.model.login.Login;
-import com.dereekb.gae.server.datastore.exception.UpdateUnkeyedEntityException;
-import com.dereekb.gae.server.datastore.objectify.components.ObjectifyKeyedSetter;
+import com.dereekb.gae.server.datastore.models.keys.ModelKey;
 import com.dereekb.gae.server.datastore.objectify.core.ObjectifyDatabaseEntity;
 import com.dereekb.gae.server.datastore.objectify.core.impl.ObjectifyDatabaseImpl;
 import com.dereekb.gae.test.applications.api.ApiApplicationTestContext;
+import com.dereekb.gae.test.applications.api.model.tests.database.SetterTestUtility.SetterTestUtilityDelegate;
+import com.dereekb.gae.utilities.misc.random.PositiveLongGenerator;
 
 public class ObjectifyDatabaseTest extends ApiApplicationTestContext {
 
@@ -28,17 +28,23 @@ public class ObjectifyDatabaseTest extends ApiApplicationTestContext {
 
 	// MARK: Setter
 	@Test
-	public void testUpdateNewItemFails() {
-		ObjectifyKeyedSetter<Login> setter = this.databaseEntity.setter();
+	public void testSetter() {
+		SetterTestUtility<Login> utility = new SetterTestUtility<Login>(this.databaseEntity,
+		        new SetterTestUtilityDelegate<Login>() {
 
-		Login test = new Login();
+			        @Override
+			        public Login makeNew() {
+				        return new Login();
+			        }
 
-		try {
-			setter.update(test);
-			Assert.fail("Update should have failed.");
-		} catch (UpdateUnkeyedEntityException e) {
+			        @Override
+			        public ModelKey makeKey() {
+				        return new ModelKey(PositiveLongGenerator.randomPositiveLong());
+			        }
 
-		}
+		        });
+
+		utility.assertTestsPass();
 	}
 
 	// MARK: Getter
