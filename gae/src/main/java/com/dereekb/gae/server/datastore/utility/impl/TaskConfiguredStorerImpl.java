b7dd5354c3619841a247603f351c536e00bc3dcd
Derek Burgman <dereekb@gmail.com>
2017-03-25 01:01:43 -0500

    Updated Configurations (Checkpoint)

---
+++ gae/src/main/java/com/dereekb/gae/server/datastore/utility/impl/TaskConfiguredStorerImpl.java
@@ -0,0 +1,68 @@
+package com.dereekb.gae.server.datastore.utility.impl;
+
+import com.dereekb.gae.server.datastore.Storer;
+import com.dereekb.gae.server.datastore.exception.StoreKeyedEntityException;
+import com.dereekb.gae.server.taskqueue.scheduler.utility.builder.TaskRequestSender;
+
+/**
+ * {@link Storer} implementation that uses a {@link TaskRequestSender}.
+ * 
+ * @author dereekb
+ *
+ * @param <T>
+ *            model type
+ */
+public class TaskConfiguredStorerImpl<T>
+        implements Storer<T> {
+
+	private Storer<T> storer;
+	private TaskRequestSender<T> taskRequestSender;
+
+	public TaskConfiguredStorerImpl(TaskRequestSender<T> taskRequestSender, Storer<T> storer) {
+		this.setTaskRequestSender(taskRequestSender);
+		this.setStorer(storer);
+	}
+
+	public TaskRequestSender<T> getTaskRequestSender() {
+		return this.taskRequestSender;
+	}
+
+	public void setTaskRequestSender(TaskRequestSender<T> taskRequestSender) {
+		if (taskRequestSender == null) {
+			throw new IllegalArgumentException("taskRequestSender cannot be null.");
+		}
+
+		this.taskRequestSender = taskRequestSender;
+	}
+
+	public Storer<T> getStorer() {
+		return this.storer;
+	}
+
+	public void setStorer(Storer<T> storer) {
+		if (storer == null) {
+			throw new IllegalArgumentException("storer cannot be null.");
+		}
+
+		this.storer = storer;
+	}
+
+	// MARK: Storer
+	@Override
+	public void store(T entity) throws StoreKeyedEntityException {
+		this.storer.store(entity);
+		this.taskRequestSender.sendTask(entity);
+	}
+
+	@Override
+	public void store(Iterable<T> entities) throws StoreKeyedEntityException {
+		this.storer.store(entities);
+		this.taskRequestSender.sendTasks(entities);
+	}
+
+	@Override
+	public String toString() {
+		return "TaskConfiguredStorer [taskRequestSender=" + this.taskRequestSender + ", storer=" + this.storer + "]";
+	}
+
+}
