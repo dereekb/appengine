9861d091867eb8874eeeaa9d429e81749f7ee2a8
Derek Burgman <dereekb@gmail.com>
2017-04-17 23:27:02 -0500

    Added TestTaskQueueController

---
+++ gae/src/main/java/com/dereekb/gae/web/taskqueue/TestTaskQueueController.java
@@ -0,0 +1,46 @@
+package com.dereekb.gae.web.taskqueue;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dereekb.gae.web.api.exception.ApiCaughtRuntimeException;
+import com.dereekb.gae.web.api.shared.response.ApiResponse;
+import com.dereekb.gae.web.api.shared.response.ApiResponseData;
+import com.dereekb.gae.web.api.shared.response.impl.ApiResponseDataImpl;
+import com.dereekb.gae.web.api.shared.response.impl.ApiResponseImpl;
+
+/**
+ * Controller use for testing to see if the taskqueue is accessible.
+ * 
+ * @author dereekb
+ *
+ */
+@RestController
+@RequestMapping("/taskqueue")
+public class TestTaskQueueController {
+
+	@ResponseBody
+	@RequestMapping(path = "/test", method = RequestMethod.GET, produces = "application/json")
+	public final ApiResponse getStatus() throws ApiCaughtRuntimeException {
+		ApiResponseImpl response = new ApiResponseImpl();
+
+		ApiResponseData data = new ApiResponseDataImpl("Success", "Success");
+		response.setData(data);
+
+		return response;
+	}
+
+	@ResponseBody
+	@RequestMapping(path = "/test", method = RequestMethod.PUT, produces = "application/json")
+	public final ApiResponse putStatus() throws ApiCaughtRuntimeException {
+		ApiResponseImpl response = new ApiResponseImpl();
+
+		ApiResponseData data = new ApiResponseDataImpl("Success", "Success");
+		response.setData(data);
+
+		return response;
+	}
+
+}
