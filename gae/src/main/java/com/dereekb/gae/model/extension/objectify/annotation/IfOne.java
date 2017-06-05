3a284e26f590b6232d9ade6a9a443bb59cea8f90
Derek Burgman <dereekb@gmail.com>
2017-06-05 14:14:07 -0500

    Added IfOne Annotation

---
+++ gae/src/main/java/com/dereekb/gae/model/extension/objectify/annotation/IfOne.java
@@ -0,0 +1,18 @@
+package com.dereekb.gae.model.extension.objectify.annotation;
+
+import com.googlecode.objectify.condition.ValueIf;
+
+/**
+ * {@link ValueIf} implementation that checks whether or not the value is 1.
+ *
+ * @author dereekb
+ *
+ */
+public class IfOne extends ValueIf<Number> {
+
+	@Override
+	public boolean matchesValue(Number value) {
+		return value != null && value.doubleValue() == 0;
+	}
+
+}
