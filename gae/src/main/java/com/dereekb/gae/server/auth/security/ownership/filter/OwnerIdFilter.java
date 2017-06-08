708eaf0a72ce9afa206d5ad6065f7b68d1e2c14f
Derek Burgman <dereekb@gmail.com>
2017-06-08 03:01:26 -0500

    Added AbstractOwnerKeyFilter
    - Added AbstractFilterFactory
    - Updated related types to implement/extend new abstract filters

--- gae/src/main/java/com/dereekb/gae/server/auth/security/ownership/filter/OwnerIdFilter.java
+++ gae/src/main/java/com/dereekb/gae/server/auth/security/ownership/filter/OwnerIdFilter.java
@@ -1,53 +1,23 @@
 package com.dereekb.gae.server.auth.security.ownership.filter;
 
 import com.dereekb.gae.server.datastore.models.owner.OwnedModel;
-import com.dereekb.gae.utilities.filters.FilterResult;
-import com.dereekb.gae.utilities.filters.impl.AbstractFilter;
-import com.dereekb.gae.utilities.filters.impl.DelegatedAbstractFilter;
 
 /**
- * Filters {@link OwnedModel}
+ * {@link AbstractOwnerKeyFilter} extension for {@link OwnedModel}.
  * 
  * @author dereekb
  *
  */
-public class OwnerIdFilter extends DelegatedAbstractFilter<OwnedModel> {
+public class OwnerIdFilter extends AbstractOwnerKeyFilter<String, OwnedModel> {
 
 	public OwnerIdFilter(String ownerId) {
-		this.setOwnerId(ownerId);
+		super(ownerId);
 	}
 
-	public void setOwnerId(String ownerId) {
-		if (ownerId == null) {
-			super.setFilter(new NullOwnerStringFilter());
-		} else {
-			super.setFilter(new SetOwnerStringFilter(ownerId));
-		}
-	}
-
-	// MARK: Internal
-	private class NullOwnerStringFilter extends AbstractFilter<OwnedModel> {
-
-		@Override
-		public FilterResult filterObject(OwnedModel object) {
-			return FilterResult.withBoolean(object.getOwnerId() == null);
-		}
-
-	}
-
-	private class SetOwnerStringFilter extends AbstractFilter<OwnedModel> {
-
-		private final String ownerId;
-
-		public SetOwnerStringFilter(String ownerId) {
-			this.ownerId = ownerId;
-		}
-
-		@Override
-		public FilterResult filterObject(OwnedModel object) {
-			return FilterResult.withBoolean(this.ownerId.equals(object.getOwnerId()));
-		}
-
+	// MARK: AbstractOwnerKeyFilter
+	@Override
+	protected String getInputKey(OwnedModel input) {
+		return input.getOwnerId();
 	}
 
 }
--- gae/src/main/java/com/dereekb/gae/utilities/filters/impl/AbstractFactoryFilter.java
+++ gae/src/main/java/com/dereekb/gae/utilities/filters/impl/AbstractFactoryFilter.java
@@ -6,51 +6,35 @@ import com.dereekb.gae.utilities.filters.FilterResult;
 import com.dereekb.gae.utilities.filters.FilterResults;
 
 /**
- * {@link Filter} that uses a {@link AbstractFilter} to delegate methods to.
+ * Anstract {@link Filter} that creates a filter each time the input is
+ * requested.
  * 
  * @author dereekb
  *
  * @param <T>
  *            model type
  */
-public class DelegatedAbstractFilter<T>
+public abstract class AbstractFactoryFilter<T>
         implements Filter<T> {
 
-	private AbstractFilter<T> filter;
-
-	protected DelegatedAbstractFilter() {}
-
-	public DelegatedAbstractFilter(AbstractFilter<T> filter) {
-		this.setFilter(filter);
-	}
-
-	public AbstractFilter<T> getFilter() {
-		return this.filter;
-	}
-
-	public void setFilter(AbstractFilter<T> filter) {
-		if (filter == null) {
-			throw new IllegalArgumentException("Filter cannot be null.");
-		}
-
-		this.filter = filter;
-	}
-
-	// MARK: Filter
 	@Override
 	public FilterResult filterObject(T object) {
-		return this.filter.filterObject(object);
+		return this.makeFilter().filterObject(object);
 	}
 
 	@Override
 	public FilterResults<T> filterObjects(Iterable<? extends T> objects) {
-		return this.filter.filterObjects(objects);
+		return this.makeFilter().filterObjects(objects);
 	}
 
+	@Deprecated
 	@Override
 	public <W> FilterResults<W> filterObjectsWithDelegate(Iterable<? extends W> sources,
 	                                                      FilterDelegate<T, W> delegate) {
-		return this.filter.filterObjectsWithDelegate(sources, delegate);
+		return this.makeFilter().filterObjectsWithDelegate(sources, delegate);
 	}
+
+	// MARK: Internal
+	protected abstract Filter<T> makeFilter();
 
 }
