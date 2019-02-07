f012308b025c54e34d646409945d2cee292f8178
Derek Burgman <dereekb@gmail.com>
2019-02-06 23:59:27 -0600

    Jenkins Pipeline (Checkpoint)

---
+++ jenkins/scripts/artifactory-deploy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "Deploying to artifactory."
+mvn publish
--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -22,7 +22,9 @@ set -x
 VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
 set +x
 
-echo 'The following command runs and outputs the execution of your Java'
-echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
-set -x
-java -jar target/${NAME}-${VERSION}.jar
+
+
+#echo 'The following command runs and outputs the execution of your Java'
+#echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
+#set -x
+#java -jar target/${NAME}-${VERSION}.jar
