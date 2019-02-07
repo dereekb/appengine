db0571e05ec9152bbcc79fa5b243904a26dba7b1
Derek Burgman <dereekb@gmail.com>
2019-02-07 01:51:31 -0600

    Jenkins Pipeline (Checkpoint)

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 # Deploy to artifactory
-# 
 echo "Deploying to Artifactory."
 mvn deploy -DskipTests=true
--- jenkins/scripts/initialize.sh
+++ jenkins/scripts/initialize.sh
@@ -3,3 +3,6 @@ echo "Initializing Maven.";
 
 # Copy Maven settings to the repo
 cp ./jenkins/maven/settings.xml /root/.m2/settings.xml
+
+# Install Dependencies
+mvn install
