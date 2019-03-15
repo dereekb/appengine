e32049d68f7e91a2ae7c02a0a2bd890d02192070
Derek Burgman <dereekb@gmail.com>
2019-03-15 02:48:15 -0500

    Web Testings (Checkpoint)

--- jenkins/scripts/appengine-web/initialize.sh
+++ jenkins/scripts/appengine-web/initialize.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 echo "Initializing Angular/npm.";
 
-# Set NPM Repository
-sh -c "npm config set registry ${NPM_REGISTRY_URL}"
-
 # Login To Repository
+echo ${VERDACCIO_LOGIN_USR}
+echo ${VERDACCIO_LOGIN_PWD}
+echo ${NPM_REGISTRY_URL}
 sh -c "npm-cli-login -r ${NPM_REGISTRY_URL} -u ${VERDACCIO_LOGIN_USR} -p ${VERDACCIO_LOGIN_PWD} -e ${NPM_EMAIL_ADDRESS}"
 
-# Print NPMRC For Testing
-cat .npmrc
+# Set NPM Repository
+sh -c "npm config set registry ${NPM_REGISTRY_URL}"
 
 # Install
 cd "./appengine-web"
--- jenkins/scripts/appengine-web/publish.sh
+++ jenkins/scripts/appengine-web/publish.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # Publish To Verdaccio
 set -e
-private_registry=${NPM_REPOSITORY_URL}
 
 # Publish
-sh -c "npm --registry $private_registry publish"
+sh -c "npm --registry ${NPM_REPOSITORY_URL} publish"
