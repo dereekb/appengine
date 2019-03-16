b98cf57ce5c800051e2f409875b82e28c343a8c5
Derek Burgman <dereekb@gmail.com>
2019-03-16 01:37:58 -0500

    Test

--- jenkins/scripts/appengine-web/initialize.sh
+++ jenkins/scripts/appengine-web/initialize.sh
@@ -7,11 +7,12 @@ echo "Logging Into Repository";
 
 # Set NPM Repository
 ##sh -c "npm set registry ${NPM_REGISTRY_URL}"
+
 # Login To Repository
 ##sh -c "npm-cli-login -r ${NPM_REGISTRY_URL} -u ${VERDACCIO_LOGIN_USR} -p ${VERDACCIO_LOGIN_PSW} -e ${NPM_EMAIL_ADDRESS}"
 
-echo "//dev.dereekb.com:10000/verdaccio/:_authToken=R+GtVHZeXKANECATWsZwxBdaYLvZru7g5JgIduDdLKCWqoodJ738DPI4WpT++/Ay" >> ~/.npmrc
-echo "registry=https://dev.dereekb.com:10000/verdaccio/" >> ~/.npmrc
+echo "//${NPM_REGISTRY_URL}:_authToken=${NPM_TOKEN}" >> ~/.npmrc
+echo "registry=${NPM_REGISTRY_URL}" >> ~/.npmrc
 # echo "always-auth=true" >> ~/.npmrc
 
 echo "Installing Node Modules";
