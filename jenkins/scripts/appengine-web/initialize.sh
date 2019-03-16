e74d24c6a31f8b2d5d789233847ef80e8d9a34b3
Derek Burgman <dereekb@gmail.com>
2019-03-15 23:00:22 -0500

    NPM Auth Test

--- jenkins/scripts/appengine-web/initialize.sh
+++ jenkins/scripts/appengine-web/initialize.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 echo "Initializing Angular/npm.";
 
-# Login To Repository
-sh -c "npm-cli-login -r ${NPM_REGISTRY_URL} -u ${VERDACCIO_LOGIN_USR} -p ${VERDACCIO_LOGIN_PWD} -e ${NPM_EMAIL_ADDRESS}"
-
-# Set NPM Repository
-sh -c "npm config set registry ${NPM_REGISTRY_URL}"
-
 # Install
 cd "./appengine-web"
 sh -c "npm install"
