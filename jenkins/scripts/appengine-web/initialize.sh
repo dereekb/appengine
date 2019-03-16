deec53e284abc36bc9cb9480a65abe0db5d65779
Derek Burgman <dereekb@gmail.com>
2019-03-16 00:55:29 -0500

    Web Testings (Checkpoint)

--- jenkins/scripts/appengine-web/initialize.sh
+++ jenkins/scripts/appengine-web/initialize.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 echo "Initializing Angular/npm.";
 
+# Set NPM Repository
+sh -c "npm set registry ${NPM_REGISTRY_URL}"
+
 # Login To Repository
 sh -c "npm-cli-login -r ${NPM_REGISTRY_URL} -u ${VERDACCIO_LOGIN_USR} -p ${VERDACCIO_LOGIN_PWD} -e ${NPM_EMAIL_ADDRESS}"
 
-# Set NPM Repository
-sh -c "npm config set registry ${NPM_REGISTRY_URL}"
+sh -c "echo ~/.npmrc"
 
 # Install
 cd "./appengine-web"
