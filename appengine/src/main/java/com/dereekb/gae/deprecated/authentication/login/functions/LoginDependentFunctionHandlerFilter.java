72a05bc6a6f63fa6e9be0554033f9b34fa03bb78
Derek Burgman <dereekb@gmail.com>
2019-06-16 00:32:38 -0500

    Deprecated Code Deletion
    - Deleted code that referenced com.google.appengine.api.users

--- appengine/src/main/java/com/dereekb/gae/deprecated/authentication/login/functions/LoginDependentFunctionHandlerFilter.java
+++
@@ -1,8 +0,0 @@
-package com.thevisitcompany.gae.deprecated.authentication.login.functions;
-
-import com.thevisitcompany.gae.deprecated.authentication.login.support.LoginDependent;
-import com.thevisitcompany.gae.utilities.function.staged.filter.StagedFunctionFilter;
-
-public interface LoginDependentFunctionHandlerFilter<T> extends StagedFunctionFilter<T>, LoginDependent {
-
-}
--- appengine/src/main/java/com/dereekb/gae/deprecated/authentication/login/support/LoginSourceService.java
+++
@@ -1,7 +0,0 @@
-package com.thevisitcompany.gae.deprecated.authentication.login.support;
-
-public interface LoginSourceService {
-
-	public LoginSource getLoginDelegate();
-
-}
