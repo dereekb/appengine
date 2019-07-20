40890cd9647e053343390937a3ada2ae9f2c49c3
Derek Burgman <dereekb@gmail.com>
2019-07-20 01:31:28 -0500

    Docker Build Fix

--- jenkins/scripts/appengine-web/publish.sh
+++ jenkins/scripts/appengine-web/publish.sh
@@ -5,24 +5,18 @@
 branch=$GIT_BRANCH
 releaseId=${BUILD_NUMBER:?no build number was available in the environment}     # Comes from Jenkins
 
-# Project Variables
-projectsFolder="./appengine-web/projects/gae-web"
-projects=( appengine-analytics appengine-api appengine-client appengine-components appengine-gateway appengine-services appengine-token appengine-utility )
-
-# Publish Each Project at the specified path
-for project in "${projects[@]}"
-do
-  # Set Our Directory
-  cd $projectsFolder/$project
+# Publish Each Project at projects path
+cd ./appengine-web/projects/gae-web
+for libName in *; do
 
   # Set the version number based on the buildId
-  if [ "$branch" = "master"]; then
-  echo "Release for master. Using existing version number."
+  if [ $branch = "master" ]; then
+  echo "Release for $libName for master. Using existing version number."
   else
-  echo "Release for pre-release build. Using release number: $releaseId"
-  sh -c "npm version prerelease --preid $releaseId"  # Set the Build Number First
+  echo "Release for $libName for pre-release build. Using release number: $releaseId"
+  sh -c "cd $libName && npm version prerelease --preid $releaseId"  # Set the Build Number First
   fi;
 
   # Publish
-  sh -c "npm publish"
+  sh -c "cd $libName && npm publish"
 done
